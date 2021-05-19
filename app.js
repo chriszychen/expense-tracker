fb9036164ada41a9db8cd297301a45105ee0a5c0
Chris Chen <ok3201123@gmail.com>
2021-05-19 15:22:27 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 
-// routes setting
+// --- routes setting ---
 app.get('/', (req, res) => {
   Promise.all([Record.find().lean().sort('-date'), Category.find().lean()])
     .then(results => {
@@ -23,12 +24,27 @@ app.get('/', (req, res) => {
     })
     .catch(err => console.log(err))
 })
+// render new page
+app.get('/records/new', (req, res) => {
+  const date = getDefaultDate()
+  res.render('new', { date })
+})
+
+// CREATE function
+app.post('/records', (req, res) => {
+  const record = req.body
+  console.log(record)
+  Record.create(record)
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
 // listen to the server
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
 
+// function
 function getIconClass(categoryName, categories) {
   const category = categories.find(category => category.name === categoryName)
   return category.iconClass
@@ -37,4 +53,12 @@ function getIconClass(categoryName, categories) {
 function getTotalAmount(records) {
   const amounts = records.map(record => record.amount)
   return amounts.reduce((sum, current) => sum + current, 0)
+}
+
+function getDefaultDate() {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = ('0' + (today.getMonth() + 1)).slice(-2)
+  const date = ('0' + today.getDate()).slice(-2)
+  return `${year}-${month}-${date}`
 }
