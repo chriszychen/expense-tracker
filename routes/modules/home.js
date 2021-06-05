a243b15205efbd204ff3455cd4d889f94de72068
Chris Chen <ok3201123@gmail.com>
2021-06-05 21:55:14 +0800

    feature: add date filter

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
-const { getAccountFormat, getTotalAmount, getInputDateString, getIconClass } = require('../../public/javascripts/functions')
+const { getAccountFormat, getTotalAmount, getInputDateString, getIconClass, getDefaultDate } = require('../../public/javascripts/functions')
 
 // render index page
 router.get('/', (req, res) => {
@@ -10,11 +10,12 @@ router.get('/', (req, res) => {
     .then(results => {
       const [records, categories] = results
       const totalAmount = getAccountFormat(getTotalAmount(records))
+      const today = getDefaultDate()
       records.forEach(record => {
         record.iconClass = getIconClass(record.category, categories)
         record.date = getInputDateString(record.date)
       })
-      res.render('index', { records, totalAmount })
+      res.render('index', { records, totalAmount, startDate: '2018-01-01', endDate: today })
     })
     .catch(err => console.log(err))
 })
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -6,15 +6,23 @@ const { getAccountFormat, getTotalAmount, getIconClass, getDefaultDate, getInput
 
 // render filtered records
 router.get('/filter', (req, res) => {
-  const categoryFilter = req.query.filter
-  Promise.all([Record.find({ category: categoryFilter }).lean().sort('-date'), Category.find().lean()])
+  const categoryFilter = req.query.category
+  let { startDate, endDate } = req.query
+  startDate = startDate || '2018-01-01'
+  endDate = endDate || getDefaultDate()
+  if (getUnixTime(startDate) > getUnixTime(endDate)) {
+    [startDate, endDate] = [endDate, startDate]
+  }
+
+  Promise.all([Record.find({ category: { $regex: categoryFilter }, date: { $gte: getUnixTime(startDate), $lt: getUnixTime(endDate) } }).lean().sort('-date'), Category.find().lean()])
     .then(results => {
       const [filteredRecords, categories] = results
       const totalAmount = getAccountFormat(getTotalAmount(filteredRecords))
       filteredRecords.forEach(record => {
         record.iconClass = getIconClass(record.category, categories)
+        record.date = getInputDateString(record.date)
       })
-      res.render('index', { records: filteredRecords, totalAmount, categoryFilter })
+      res.render('index', { records: filteredRecords, totalAmount, categoryFilter, startDate, endDate })
     })
     .catch(err => console.log(err))
 })
