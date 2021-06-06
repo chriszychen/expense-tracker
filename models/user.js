8838c1023a98ae8b25cafa7b2c2793313e616c0e
Chris Chen <ok3201123@gmail.com>
2021-06-06 10:17:16 +0800

    feat: add user model and user registration feature

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model('User', userSchema)
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.post('/login', (req, res) => {
+  console.log('login function')
 })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  try {
+    const user = await User.findOne({ email }).exec()
+    if (user) {
+      console.log('User exists')
+      return res.render('register', { name, email, password, confirmPassword })
+    } else {
+      await User.create({ name, email, password })
+      return res.redirect('/')
+    }
+  } catch (e) {
+    console.log(e)
+  }
 })
 
 module.exports = router
