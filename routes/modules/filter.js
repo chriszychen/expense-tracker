fdbceeb1dd1cf0c842290020b0656be5d7d6092f
Chris Chen <ok3201123@gmail.com>
2021-09-28 08:23:44 +0800

    refactor: set default value in destructuring

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -9,24 +9,38 @@ const { getAccountingFormat, getTotalAmount, getIconClassName } = require('../..
 router.get('/', (req, res) => {
   const userId = req.user._id
   const categoryFilter = req.query.category
-  let { startDate, endDate } = req.query
-  startDate = startDate || '2021-01-01' // default start date
-  endDate = endDate || moment().format('YYYY-MM-DD') // today as default end date
-  if ((new Date(startDate)).valueOf() > new Date(endDate).valueOf()) {
-    [startDate, endDate] = [endDate, startDate]
+  const defaultStartDate = '2021-01-01'
+  let { startDate = defaultStartDate, endDate = moment().format('YYYY-MM-DD') } = req.query // set default value if undefined
+  if (new Date(startDate).valueOf() > new Date(endDate).valueOf()) {
+    ;[startDate, endDate] = [endDate, startDate]
   }
-
-  Promise.all([Record.find({ category: { $regex: categoryFilter }, date: { $gte: startDate, $lt: endDate }, userId }).lean().sort('-date'), Category.find().lean()])
-    .then(results => {
+  // 可以再用mongoose aggregate練習query語法
+  Promise.all([
+    Record.find({
+      category: { $regex: categoryFilter },
+      date: { $gte: startDate, $lte: endDate },
+      userId,
+    })
+      .lean()
+      .sort('-date'),
+    Category.find().lean(),
+  ])
+    .then((results) => {
       const [filteredRecords, categories] = results
       const totalAmount = getAccountingFormat(getTotalAmount(filteredRecords))
-      filteredRecords.forEach(record => {
+      filteredRecords.forEach((record) => {
         record.iconClass = getIconClassName(record.category, categories)
         record.date = moment(record.date).format('YYYY-MM-DD')
       })
-      res.render('index', { records: filteredRecords, totalAmount, categoryFilter, startDate, endDate })
+      res.render('index', {
+        records: filteredRecords,
+        totalAmount,
+        categoryFilter,
+        startDate,
+        endDate,
+      })
     })
-    .catch(err => console.log(err))
+    .catch((err) => console.log(err))
 })
 
 module.exports = router
