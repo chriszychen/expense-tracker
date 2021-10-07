749534b5281984a95444c4698941bbd79f3a0de8
Chris Chen <ok3201123@gmail.com>
2021-10-07 18:33:47 +0800

    fix: change users.js to async/await and fix same email registration bug

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -22,32 +22,31 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', async (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
+  try {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
+      return res.render('register', { errors, name, email, password, confirmPassword })
+    }
+    const user = await User.findOne({ email })
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', { errors, name, email, password, confirmPassword })
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+      await User.create({ name, email, password: hash })
+      return res.redirect('/users/login')
+    }
+  } catch (err) {
+    console.log(err)
   }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符！' })
-  }
-  if (errors.length) {
-    return res.render('register', { errors, name, email, password, confirmPassword })
-  }
-
-  User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        errors.push({ message: '這個 Email 已經註冊過了。' })
-        return res.render('register', { name, email, password, confirmPassword })
-      } else {
-        return bcrypt
-          .genSalt(10)
-          .then((salt) => bcrypt.hash(password, salt))
-          .then((hash) => User.create({ name, email, password: hash }))
-          .then(() => res.redirect('/users/login'))
-      }
-    })
-    .catch((err) => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
