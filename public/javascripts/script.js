a5234644d97362106477c86d64d25328ce7b5aef
Chris Chen <ok3201123@gmail.com>
2021-10-25 09:25:17 +0800

    fix: fix frontend script console error

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,33 +3,39 @@ const filterForm = document.querySelector('#filter-form')
 const startDateInput = document.querySelector('#start-date-input')
 const endDateInput = document.querySelector('#end-date-input')
 
-startDateInput.addEventListener('change', (event) => {
-  if (startDateInput.value > endDateInput.value) {
-    window.alert('The start date should be earlier or the same as the end date.')
-    filterForm.submit()
-  } else {
-    filterForm.submit()
-  }
-})
+if (startDateInput) {
+  startDateInput.addEventListener('change', (event) => {
+    if (startDateInput.value > endDateInput.value) {
+      window.alert('The start date should be earlier or the same as the end date.')
+      filterForm.submit()
+    } else {
+      filterForm.submit()
+    }
+  })
+}
 
-endDateInput.addEventListener('change', (event) => {
-  if (endDateInput.value < startDateInput.value) {
-    window.alert('The end date should be later or the same as the start date.')
-    filterForm.submit()
-  } else {
-    filterForm.submit()
-  }
-})
+if (endDateInput) {
+  endDateInput.addEventListener('change', (event) => {
+    if (endDateInput.value < startDateInput.value) {
+      window.alert('The end date should be later or the same as the start date.')
+      filterForm.submit()
+    } else {
+      filterForm.submit()
+    }
+  })
+}
 
-recordPanel.addEventListener('click', (event) => {
-  if (event.target.matches('.delete-button')) {
-    const button = event.target
-    setModalData(button)
-  } else if (event.target.matches('.fa-trash-alt')) {
-    const button = event.target.parentElement
-    setModalData(button)
-  }
-})
+if (recordPanel) {
+  recordPanel.addEventListener('click', (event) => {
+    if (event.target.matches('.delete-button')) {
+      const button = event.target
+      setModalData(button)
+    } else if (event.target.matches('.fa-trash-alt')) {
+      const button = event.target.parentElement
+      setModalData(button)
+    }
+  })
+}
 
 function setModalData(button) {
   const modalTitle = document.querySelector('#modal-title')
