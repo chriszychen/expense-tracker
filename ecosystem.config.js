c9b1a2a83992b49a99fab690118c5cb3eff4d08f
Chris Chen <ok3201123@gmail.com>
2021-11-23 07:52:01 +0800

    fix: modify pm2 comfig

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'app',
       script: './app.js',
-      instances: '2',
-      exec_mode: 'cluster',
+      instances: '1',
+      // exec_mode: 'cluster',
       max_memory_restart: '300M',
       env: {
         NODE_ENV: 'development',
