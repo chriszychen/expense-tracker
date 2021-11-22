4f2beb427436a6ac75fc7c91fca1eb8a1700348d
Chris Chen <ok3201123@gmail.com>
2021-11-23 01:27:09 +0800

    feat: add pm2 and config file

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app',
+      script: './app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      watch: '.',
+      ignore_watch: 'node_modules',
+      max_memory_restart: '300M',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
